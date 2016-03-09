5dca79308c455a4467194d33c2deafaf4e5b7ce7
Connor Sedwick <connorsedwick@10-249-37-149.wireless.oregonstate.edu>
2016-03-09 15:03:15 -0800

    Added Player class and Tests

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -4,4 +4,35 @@ package models;
  * Created by nathan on 3/8/16.
  */
 public class Player extends User {
+    private int Bet;
+    private int Money;
+    private int Split;
+
+    public Player(){
+        Money = 200;
+        Bet = 2;
+        Split = 0;
+    }
+
+    public void setBet(int amount){
+        if(amount <= getMoney()) {
+            Bet = amount;
+        }
+    }
+
+    public void loseBet(){
+        Money = Money - Bet;
+    }
+
+    public void winBet(){
+        Money = Money + Bet;
+    }
+
+    public int getBet(){
+        return Bet;
+    }
+
+    public int getMoney(){
+        return Money;
+    }
 }
---
+++ src/test/java/models/testPlayer.java
@@ -0,0 +1,74 @@
+package models;
+
+import org.junit.Test;
+import org.junit.runners.Suite;
+import static org.junit.Assert.*;
+
+/**
+ * Created by connorsedwick on 3/9/16.
+ */
+public class testPlayer {
+
+    @Test
+    public void testPlayer(){
+        Player p = new Player();
+        assertNotNull(p);
+    }
+
+    @Test
+    public void testWin(){
+        Player p = new Player();
+
+        //Check original amount
+        assertEquals(200, p.getMoney());
+
+        //Add to money
+        p.winBet();
+        assertEquals(202, p.getMoney());
+    }
+
+    @Test
+    public void testMoney(){
+        Player p = new Player();
+        assertEquals(200, p.getMoney());
+    }
+
+    @Test
+    public void testBet(){
+        Player p = new Player();
+        Player p2 = new Player();
+
+        //Default bet
+        assertEquals(2, p.getBet());
+        assertEquals(p2.getBet(), p.getBet());
+
+        //User-defined bet above amount on hand
+        p2.setBet(210);
+        assertEquals(p2.getBet(), p.getBet());
+
+        //User-defined bet at amount on hand
+        p2.setBet(200);
+        assertNotEquals(p2.getBet(), p.getBet());
+
+        //User-defined bet below amount on hand
+        p2.setBet(187);
+        assertNotEquals(p2.getBet(), p.getBet());
+    }
+
+    @Test
+    public void testWin_Lose(){
+        Player p = new Player();
+        Player p2 = new Player();
+
+        //Check to see if winning increases money available
+        p.setBet(20);
+        p.winBet();
+        assertNotEquals(p.getMoney(),p2.getMoney());
+
+        //Check to see if losing decreases money available
+        p.loseBet();
+        assertEquals(p.getMoney(),p2.getMoney());
+    }
+
+
+}
